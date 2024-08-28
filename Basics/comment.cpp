11e624994f5d48b71f3cae6dd14a7bfc47d72e64
Rutuja Pawale <160813855+RutujaPawale@users.noreply.github.com>
2024-08-28 22:14:04 +0530

    Update comment.cpp

--- Basics/comment.cpp
+++ Basics/comment.cpp
@@ -1,8 +1,19 @@
 #include<iostream>
 using namespace std;
 
+
+//this is a single line comment
+
+
 int main()
 {
     cout<<"Hello World!";
     return 0;
 }
+
+
+/*
+This is a 
+multiline comment
+!!!!!!!!!!!!!!!!
+*/
