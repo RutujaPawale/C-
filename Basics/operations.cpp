4eb8bb14debf12edaabc9fba5dff7f9504370332
Rutuja Pawale <160813855+RutujaPawale@users.noreply.github.com>
2024-08-28 22:26:47 +0530

    Update operations.cpp

--- Basics/operations.cpp
+++ Basics/operations.cpp
@@ -1,1 +1,12 @@
+#include<iostream>
+using namespace std;
 
+int a=10, b=5;
+int main()
+{
+    cout<<"a + b = "<<a+b<<endl;
+    cout<<"a - b = "<<a-b<<endl;
+    cout<<"a * b = "<<a*b<<endl;
+    cout<<"a / b = "<<a/b<<endl;
+    return 0;
+}
