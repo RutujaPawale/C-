bc6354c6aebc80a76acb2b64d39ed6122c973d40
Rutuja Pawale <160813855+RutujaPawale@users.noreply.github.com>
2024-08-29 19:11:36 +0530

    Update for_loop.cpp

--- Loops/for_loop.cpp
+++ Loops/for_loop.cpp
@@ -1,3 +1,13 @@
-for (int i = 0; i < 5; i++) {
-  cout << i << "\n";
+#include<iostream>
+using namespace std;
+
+int i=1;
+int main()
+{
+    for (int i = 0; i < 5; i++) 
+    {
+        cout << i << "\n";
+    }
+    return 0;
 }
+
