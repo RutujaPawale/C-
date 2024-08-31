58188eb3aef6a28665b4df12a3eb9f0bc96c68f7
Rutuja Pawale <160813855+RutujaPawale@users.noreply.github.com>
2024-08-31 22:17:48 +0530

    Update continue.cpp

--- Loops/continue.cpp
+++ Loops/continue.cpp
@@ -2,3 +2,14 @@
 using namespace std;
 
 int main()
+{
+    for(int i=0;i<40;i++)
+    {
+        if(i==5)
+        {
+            continue;
+        }
+        cout<<i<<endl;
+    }
+    return 0;
+}
